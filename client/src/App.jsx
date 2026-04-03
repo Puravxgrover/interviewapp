e7e25da2034ccda2ad50e23bb7be380afe6eb538
Akshay bajaj <157092472+Akshaybajaj78@users.noreply.github.com>
2026-04-03 21:15:37 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewPage from "./pages/InterviewPage";
 import InterviewHistory from "./pages/InterviewHistory";
 import Pricing from "./pages/Pricing";
 import InterviewReport from "./pages/InterviewReport";
-export const ServerUrl = "http://ai-intern.onrender.com"
+export const ServerUrl = "https://ai-intern.onrender.com"
 
 function App() {
   const dispatch = useDispatch()

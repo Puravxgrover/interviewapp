cc0fac0c5112f7d0e70c87ba1972ec79cde978c3
Puravxgrover <puravxgrover@gmail.com>
2026-06-19 12:57:56 +0530

    Update ServerUrl to production URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,8 @@ import InterviewPage from "./pages/InterviewPage";
 import InterviewHistory from "./pages/InterviewHistory";
 import Pricing from "./pages/Pricing";
 import InterviewReport from "./pages/InterviewReport";
-export const ServerUrl = "http://localhost:8080"
+export const ServerUrl = "http://localhost:8080
+export const ServerUrl = "https://interviewapp-dozz.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
